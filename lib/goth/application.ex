830674355e9aeaca900b47b9664c8d8c53e5b4c6
arkadiy kraportov <arkadiyk@gmail.com>
2024-09-08 13:28:01 +0800

    Add proxy option

--- lib/goth/application.ex
+++ lib/goth/application.ex
@@ -2,14 +2,25 @@ defmodule Goth.Application do
   @moduledoc false
   use Application
 
+  Application.put_env(:goth, :proxy, "http://myproxy.example.com:8080")
+
   @impl true
   def start(_type, _args) do
     children = [
       {Registry, keys: :unique, name: Goth.Registry},
-      {Finch, name: Goth.Finch},
+      {Finch, finch_config()},
       Goth.TokenStore
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: Goth.Supervisor)
+  end
+
+  defp finch_config do
+    if proxy = Application.get_env(:goth, :proxy) do
+      %{scheme: scheme, host: host, port: port} = URI.parse(proxy)
+      [name: Goth.Finch, pools: [default: [conn_opts: [proxy: {String.to_existing_atom(scheme), host, port, []}]]]]
+    else
+      [name: Goth.Finch]
+    end
   end
 end
